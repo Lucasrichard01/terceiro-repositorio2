5de8cdb08f3eb27b7cb6bdcebb8196a07fcf8bc2
Lucasrichard01 <lucas.ortiz.andrade@escola.pr.gov.br>
2025-06-30 08:17:25 -0300

    aula30/06/2025

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ function diminuiTamanho() {
 }
 
 function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
+    if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-const campoSenha = document.querySelector(`#campo-senha`);
+const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -31,25 +31,28 @@ const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
 
-
-
 geraSenha();
 
-
 function geraSenha() {
-let alfabeto = '';
-if (checkbox[0].cheked){
-    alfabeto = alfabeto + letrasMaiusculas;
-}
-if (checkbox[1].cheked){
-    alfabeto = alfabeto + letrasMinusculas;
-}
-
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
     }
-    campoSenha.value = senha;
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+
+    let senha = '' ;
+    for(let i=0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha; 
 }
